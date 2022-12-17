92991dd11b3a634d818fb9d9ba40962909fbec87
kernogo <kernogok@mail.ru>
2022-12-17 17:32:30 +0700

    Change API URLs

--- tg_bot/api.py
+++ tg_bot/api.py
@@ -1,11 +1,12 @@
 import requests
 
-API_URL = 'http://localhost:8080/api/v1'
+STOCKS_API_URL = 'http://localhost:8080'
+PREDICTION_API_URL = 'http://localhost:8000'
 
 
 def api_get_stocks(page: int = 1, prefix: str = None) -> requests.Response:
-    return requests.get(f'{API_URL}/stocks', params={'page': page, 'query': prefix})
+    return requests.get(f'{STOCKS_API_URL}/stocks', params={'page': page, 'query': prefix})
 
 
 def api_get_prediction(ticker: str, date: str) -> requests.Response:
-    return requests.get(f'{API_URL}/forecast/' + ticker, params={'date': date})
+    return requests.get(f'{PREDICTION_API_URL}/forecast/' + ticker, params={'date': date})
